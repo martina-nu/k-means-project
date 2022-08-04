b5c04fffe916d3d3c67ca95deb981ba24fde7e69
martina-nu <martinanu98@gmail.com>
2022-08-04 16:45:00 +0000

    Add solution

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Template for Machine Learning projects
+# K-Means Project
+
+
+**In this small project you will use the k-means algorithm to segment houses based on their coordinates and median income.**
+
+
+We will create 6 housing clusters based only on their 'latitude','longitude' and their 'medincome' column.
+
+Then we will find the optimal number of clusters
--- src/app.py
+++ src/app.py
@@ -1,4 +1,34 @@
-from utils import db_connect
-engine = db_connect()
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.metrics import silhouette_score
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
 
-# your code here
+
+url = 'https://raw.githubusercontent.com/4GeeksAcademy/k-means-project-tutorial/main/housing.csv'
+df_raw = pd.read_csv(url)
+
+df = df_raw.loc[:, ["MedInc", "Latitude", "Longitude"]]
+
+scaler = StandardScaler() 
+scaled_df = scaler.fit_transform(df) 
+
+scaled_df = pd.DataFrame(scaled_df,columns=df.columns) # use this df when k=6
+
+scaled_df2 = scaled_df.copy() #find optimal k (2)
+
+# 6 clusters
+
+kmeans = KMeans(n_clusters=6)
+scaled_df["Cluster"] = kmeans.fit_predict(scaled_df)
+scaled_df["Cluster"] = scaled_df["Cluster"].astype("category")
+
+#Optimal number of clusters: 2
+
+kmeans = KMeans(n_clusters=2)
+scaled_df2["Cluster"] = kmeans.fit_predict(scaled_df2)
+
+scaled_df2["Cluster"] = scaled_df2["Cluster"].astype("category")
+
+
